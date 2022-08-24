ff2f58d889998e0159c3f861028e80cde083d1e0
Andres Valencia <andresvalencia.diaz@davita.com>
2022-08-24 17:17:17 -0500

    feat(shared-react-ui): adding shared footer comp

--- libs/shared/react-ui/src/index.ts
+++ libs/shared/react-ui/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/shared-react-ui';
 export * from './lib/components/header/header';
 export * from './lib/components/header/nav/nav';
+export * from './lib/components/footer/footer';
