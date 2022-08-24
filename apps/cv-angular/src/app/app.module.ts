6fbb19183c67d19428f226c44b42270f91ccdc63
Andres Valencia <andresvalencia.diaz@davita.com>
2022-08-24 17:20:18 -0500

    feat(apps): updating app component

--- apps/cv-angular/src/app/app.module.ts
+++ apps/cv-angular/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
+import { SharedUiModule } from '@cv-portfolio/shared/ui';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, SharedUiModule],
   providers: [],
   bootstrap: [AppComponent],
 })
