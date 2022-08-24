46396203ddf71e7d11e536c480ca23508788ddb5
Andres Valencia <andresvalencia.diaz@davita.com>
2022-08-24 17:17:53 -0500

    feat(data): creating skill and response models

--- libs/data/src/index.ts
+++ libs/data/src/index.ts
@@ -1,3 +1,7 @@
 export * from './lib/job';
 export * from './lib/job-dto';
 export * from './lib/job-model';
+export * from './lib/skill';
+export * from './lib/skill-dto';
+export * from './lib/response-dto';
+export * from './lib/response';
---
+++ libs/data/src/lib/skill-dto.spec.ts
@@ -0,0 +1,7 @@
+import { SkillDTO } from './skill-dto';
+
+describe('SkillDTO', () => {
+  it('should create an instance', () => {
+    expect(new SkillDTO()).toBeTruthy();
+  });
+});
