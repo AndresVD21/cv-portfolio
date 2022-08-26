ba23d3cbc642784f059d38b5d229487f63711091
Andres Valencia <andresvalencia.diaz@davita.com>
2022-08-26 11:07:01 -0500

    fix(apps-cv-react): removing footer

--- apps/cv-react/src/app/app.tsx
+++ apps/cv-react/src/app/app.tsx
@@ -3,19 +3,15 @@ import './app.scss';
 import { CvReactFeatureMain } from '@cv-portfolio/cv-react/feature-main';
 import { CvReactFeatureSkills } from '@cv-portfolio/cv-react/feature-skills';
 import { Route, Routes } from 'react-router-dom';
-import { Footer } from '@cv-portfolio/shared/react-ui';
 
 export function App() {
   return (
-    <>
-      <div className="cv-portfolio-container">
-        <Routes>
-          <Route path="/" element={<CvReactFeatureMain />} />
-          <Route path="skills" element={<CvReactFeatureSkills />} />
-        </Routes>
-      </div>
-      <Footer />
-    </>
+    <div className="cv-portfolio-container">
+      <Routes>
+        <Route path="/" element={<CvReactFeatureMain />} />
+        <Route path="skills" element={<CvReactFeatureSkills />} />
+      </Routes>
+    </div>
   );
 }
 
