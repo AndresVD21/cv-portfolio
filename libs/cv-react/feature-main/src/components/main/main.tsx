f4cf8a00def9d16baca278c81658dad7dfed5b08
Andres Valencia <andresvalencia.diaz@davita.com>
2022-08-26 12:01:24 -0500

    fix(cv-react-f-main): reorganizing header and footer in main

--- libs/cv-react/feature-main/src/components/main/main.tsx
+++ libs/cv-react/feature-main/src/components/main/main.tsx
@@ -10,6 +10,7 @@ import Jobs from './jobs/jobs';
 import { useEffect, useState } from 'react';
 import { getJobHistory } from '../../services/jobs.service';
 import { JobModel } from '@cv-portfolio/data';
+import { Footer, Header } from '@cv-portfolio/shared/react-ui';
 
 /* eslint-disable-next-line */
 export interface MainProps {}
@@ -29,27 +30,43 @@ export function Main(props: MainProps) {
   };
 
   return (
-    <div className="cv-portfolio-main-container">
-      <section id="about-me" className="about container">
-        <h2 className="section-title">
-          <FontAwesomeIcon icon={faAddressCard} className="icon" />
-          About me
-        </h2>
-        <h3 className="role">Frontend Developer</h3>
-        <p className="introduction">
-          Web developer with five years of experience in front-end development,
-          focused in making high scalable web applications with the best
-          practices using the angular framework, also with abilities of working
-          with agile methodologies and self learning.
-        </p>
-      </section>
-      <section id="employment-history" className="container">
-        <h2 className="section-title">
-          <FontAwesomeIcon icon={faFolderOpen} className="icon" />
-          Jobs
-        </h2>
-        <Jobs jobs={jobs} />
-      </section>
+    <div className="cv-portfolio-main-container feature-container">
+      <Header>
+        <div className="name-container">
+          <h1 className="name">Andres Valencia Diaz</h1>
+          <p className="resume">
+            Nice to meet you! I'm Andrés from Colombia
+            <span role="img" aria-label="colombia flag">
+              🇨🇴
+            </span>
+            , Frontend developer who really likes to learn and try new things in
+            this vast world of technology!
+          </p>
+        </div>
+      </Header>
+      <div className="wrapper">
+        <section id="about-me" className="about container">
+          <h2 className="section-title">
+            <FontAwesomeIcon icon={faAddressCard} className="icon" />
+            About me
+          </h2>
+          <h3 className="role">Frontend Developer</h3>
+          <p className="introduction">
+            Web developer with five years of experience in front-end
+            development, focused in making high scalable web applications with
+            the best practices using the angular framework, also with abilities
+            of working with agile methodologies and self learning.
+          </p>
+        </section>
+        <section id="employment-history" className="container">
+          <h2 className="section-title">
+            <FontAwesomeIcon icon={faFolderOpen} className="icon" />
+            Jobs
+          </h2>
+          <Jobs jobs={jobs} />
+        </section>
+      </div>
+      <Footer />
     </div>
   );
 }
--- libs/cv-react/feature-main/src/lib/cv-react-feature-main.tsx
+++ libs/cv-react/feature-main/src/lib/cv-react-feature-main.tsx
@@ -1,4 +1,3 @@
-import { Footer, Header } from '@cv-portfolio/shared/react-ui';
 import Main from '../components/main/main';
 
 import './cv-react-feature-main.scss';
@@ -7,25 +6,7 @@ import './cv-react-feature-main.scss';
 export interface CvReactFeatureMainProps {}
 
 export function CvReactFeatureMain(props: CvReactFeatureMainProps) {
-  return (
-    <>
-      <Header>
-        <div className="name-container">
-          <h1 className="name">Andres Valencia Diaz</h1>
-          <p className="resume">
-            Nice to meet you! I'm Andrés from Colombia
-            <span role="img" aria-label="colombia flag">
-              🇨🇴
-            </span>
-            , Frontend developer who really likes to learn and try new things in
-            this vast world of technology!
-          </p>
-        </div>
-      </Header>
-      <Main />
-      <Footer />
-    </>
-  );
+  return <Main />;
 }
 
 export default CvReactFeatureMain;
