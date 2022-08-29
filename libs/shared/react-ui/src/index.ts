2d9e80c65c6ac5e0a6ee4e8281cc600ccbb48e14
Andres Valencia <andresvalencia.diaz@davita.com>
2022-08-29 10:45:42 -0500

    feat(shared-react-ui): adding space component

--- libs/shared/react-ui/src/index.ts
+++ libs/shared/react-ui/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/components/space/space';
 export * from './lib/shared-react-ui';
 export * from './lib/components/header/header';
 export * from './lib/components/header/nav/nav';
