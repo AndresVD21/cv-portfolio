044667cefddcc19dd66db2d048cb024c747d4b31
Andres Valencia <andresvalencia.diaz@davita.com>
2022-08-29 19:21:34 -0500

    fix: adding exports to shared components

--- libs/shared/ui/src/index.ts
+++ libs/shared/ui/src/index.ts
@@ -5,3 +5,5 @@ export * from './lib/angular-components/button/button.component';
 export * from './lib/angular-components/nav/nav.component';
 
 export * from './lib/angular-components/header/header.component';
+export * from './lib/angular-components/footer/footer.component';
+export * from './lib/pipes/fill.pipe';
