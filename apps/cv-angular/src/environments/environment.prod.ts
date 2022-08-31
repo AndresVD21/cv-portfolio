9ebb79d5fdc6db8666b1e1ade8394bd0ef044663
Andres Valencia <andresvalencia.diaz@davita.com>
2022-08-31 13:31:15 -0500

    fix(cv-angular): removing slash

--- apps/cv-angular/src/environments/environment.prod.ts
+++ apps/cv-angular/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://cv-portfolio-api-next-git-develop-andresvd21.vercel.app/',
+  apiUrl: 'https://cv-portfolio-api-next-git-develop-andresvd21.vercel.app',
 };
