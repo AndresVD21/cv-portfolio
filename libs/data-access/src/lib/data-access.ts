e2617d4a1167c0cabacf490e45fe17847d0d5f61
Andres Valencia <andresvalencia.diaz@davita.com>
2022-09-12 17:38:24 -0500

    fix(data-access): changing request to return observables

--- libs/data-access/src/lib/data-access.ts
+++ libs/data-access/src/lib/data-access.ts
@@ -7,52 +7,59 @@ import {
   SkillDTO,
 } from '@cv-portfolio/data';
 import { mapJobs } from '../mappers/mapJobs';
+import { from } from 'rxjs';
 
 export const getSkills = (firebaseUrl: string) =>
-  axios
-    .get(`${firebaseUrl}/getSkills`)
-    .then((res) => {
-      const response: ResponseDTO<SkillDTO[]> = {
-        data: res.data,
-      };
-      return response;
-    })
-    .catch((err) => {
-      const error: ResponseDTO<SkillDTO[]> = {
-        error: err,
-      };
-      return error;
-    });
+  from(
+    axios
+      .get(`${firebaseUrl}/getSkills`)
+      .then((res) => {
+        const response: ResponseDTO<SkillDTO[]> = {
+          data: res.data,
+        };
+        return response;
+      })
+      .catch((err) => {
+        const error: ResponseDTO<SkillDTO[]> = {
+          error: err,
+        };
+        return error;
+      })
+  );
 
 export const getLanguages = (firebaseUrl: string) =>
-  axios
-    .get(`${firebaseUrl}/getLanguages`)
-    .then((res) => {
-      const response: ResponseDTO<LanguageDTO[]> = {
-        data: res.data,
-      };
-      return response;
-    })
-    .catch((err) => {
-      const error: ResponseDTO<LanguageDTO[]> = {
-        error: err,
-      };
-      return error;
-    });
+  from(
+    axios
+      .get(`${firebaseUrl}/getLanguages`)
+      .then((res) => {
+        const response: ResponseDTO<LanguageDTO[]> = {
+          data: res.data,
+        };
+        return response;
+      })
+      .catch((err) => {
+        const error: ResponseDTO<LanguageDTO[]> = {
+          error: err,
+        };
+        return error;
+      })
+  );
 
 export const getJobs = (firebaseUrl: string) =>
-  axios
-    .get<FbJobDto[]>(`${firebaseUrl}/getJobs`)
-    .then((res) => {
-      console.log(res.data);
-      const response: ResponseDTO<JobDTO[]> = {
-        data: mapJobs(res.data),
-      };
-      return response;
-    })
-    .catch((err) => {
-      const error: ResponseDTO<JobDTO[]> = {
-        error: err,
-      };
-      return error;
-    });
+  from(
+    axios
+      .get<FbJobDto[]>(`${firebaseUrl}/getJobs`)
+      .then((res) => {
+        console.log(res.data);
+        const response: ResponseDTO<JobDTO[]> = {
+          data: mapJobs(res.data),
+        };
+        return response;
+      })
+      .catch((err) => {
+        const error: ResponseDTO<JobDTO[]> = {
+          error: err,
+        };
+        return error;
+      })
+  );
